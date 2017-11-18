3e60055b0e1212835ac926154d1c2103ae0ab418
Yury Makarov <lvoursl@gmail.com>
2017-11-18 20:40:45 +0300

    added img to text convertor using tessaract

---
+++ img_to_text.py
@@ -0,0 +1,28 @@
+# import the necessary packages
+from PIL import Image
+import pyocr
+import pyocr.builders
+
+LANG = 'rus'
+
+def covert_img_to_text(img_name, check_rotation=True, verbose=False):
+    try:
+        tools = pyocr.get_available_tools()
+        tool = tools[0]
+
+        img = Image.open(img_name)
+
+        if check_rotation:
+            orientation = tool.detect_orientation(
+                img,
+                lang=LANG
+            )            
+            text = tool.image_to_string(img.rotate(orientation['angle']), lang=LANG)
+
+        text = tool.image_to_string(img, lang=LANG)
+
+        return text
+
+    except Exception as e:
+        print(str(e))
+        return None
