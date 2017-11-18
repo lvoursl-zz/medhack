572d3a4484c8a0cafbf5c8c994faf145b63e8507
Yury Makarov <lvoursl@gmail.com>
2017-11-18 21:45:11 +0300

    added exception handling

--- create_text_dataset.py
+++ create_text_dataset.py
@@ -14,12 +14,15 @@ print(image_directry, text_directry)
 
 for root, sub_folder, files in os.walk(image_directry):
     for item in files:
-        folder = os.path.basename(os.path.normpath(root))
+        try:
+            folder = os.path.basename(os.path.normpath(root))
 
-        img_file_path = str(os.path.join(root, item))
-        text_file_path = str(os.path.join(text_directry, folder, item.replace(os.path.splitext(img_file_path)[1], '.txt')))
-        print(img_file_path, text_file_path)
-        text = convert_img_to_text(img_file_path)
+            img_file_path = str(os.path.join(root, item))
+            text_file_path = str(os.path.join(text_directry, folder, item.replace(os.path.splitext(img_file_path)[1], '.txt')))
+            print(img_file_path, text_file_path)
+            text = convert_img_to_text(img_file_path)
 
-        with open(text_file_path, 'w') as file:
-            file.write(text)
+            with open(text_file_path, 'w') as file:
+                file.write(text)
+        except Exception as e:
+            print(str(e))
