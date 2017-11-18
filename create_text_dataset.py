44d7301f519988efd26249d4b45388cd8cae7ffd
Yury Makarov <lvoursl@gmail.com>
2017-11-18 21:18:55 +0300

    added dataset creating pipeline

--- create_text_dataset.py
+++ create_text_dataset.py
@@ -14,8 +14,10 @@ print(image_directry, text_directry)
 
 for root, sub_folder, files in os.walk(image_directry):
     for item in files:
+        folder = os.path.basename(os.path.normpath(root))
+
         img_file_path = str(os.path.join(root, item))
-        text_file_path = str(os.path.join(text_directry, item.replace(os.path.splitext(img_file_path)[1], '.txt')))
+        text_file_path = str(os.path.join(text_directry, folder, item.replace(os.path.splitext(img_file_path)[1], '.txt')))
         print(img_file_path, text_file_path)
         text = convert_img_to_text(img_file_path)
 
