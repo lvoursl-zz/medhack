e9209fa17fbbcee7f3f88b55db07e86ab1fa9981
Yury Makarov <lvoursl@gmail.com>
2017-11-18 21:06:05 +0300

    added code for data creation

---
+++ create_text_dataset.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+from img_to_text import convert_img_to_text
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image_directry', type=str)
+parser.add_argument('text_directry', type=str)
+
+args = parser.parse_args()
+image_directry = args.image_directry
+text_directry = args.text_directry
+
+print(image_directry, text_directry)
+
+for root, sub_folder, files in os.walk(image_directry):
+    for item in files:
+        img_file_path = str(os.path.join(root, item))
+        text_file_path = str(os.path.join(text_directry, item.replace(os.path.splitext(img_file_path)[1], '.txt')))
+        print(img_file_path, text_file_path)
+        text = convert_img_to_text(img_file_path)
+
+        with open(text_file_path, 'w') as file:
+            file.write(text)
