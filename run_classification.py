5e7c134ebeb29a7e748ed907ec122001d072fa4b
Yury Makarov <lvoursl@gmail.com>
2017-11-19 15:56:28 +0300

    add cache

--- run_classification.py
+++ run_classification.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+from time import sleep
+
 from predict import predict
 
 parser = argparse.ArgumentParser()
@@ -18,20 +20,24 @@ try:
     # os.system('php api/request.php ' + img_file_path + ' ' + text_file_path)
 
     # call for ABBY
-    os.system('python2 abby-python-api/process.py ' + image_filename + ' ' + text_filename)
-    print('OCR finished')
-    with open(text_filename, 'r') as f:
-        text = f.read()
-        print('file loaded, start prediction')
-        predictions = predict(text)
-        print(predictions)
+    if result_filename in os.listdir('.'):
+        print('file exist!')
+        sleep(4)
+    else
+        os.system('python2 abby-python-api/process.py ' + image_filename + ' ' + text_filename)
+        print('OCR finished')
+        with open(text_filename, 'r') as f:
+            text = f.read()
+            print('file loaded, start prediction')
+            predictions = predict(text)
+            print(predictions)
 
-    with open(result_filename, 'w') as f:
-        f.write(str(predictions))
+        with open(result_filename, 'w') as f:
+            f.write(str(predictions))
 
-    # call for tesaract
-    # text = convert_img_to_text(img_file_path)
-    # with open(text_file_path, 'w') as file:
-    #     file.write(text)
+        # call for tesaract
+        # text = convert_img_to_text(img_file_path)
+        # with open(text_file_path, 'w') as file:
+        #     file.write(text)
 except Exception as e:
     print(str(e))
