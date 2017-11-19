f7092e56985bcb9a4dd9d8bfe3dfdce2be27d176
Yury Makarov <lvoursl@gmail.com>
2017-11-19 12:53:10 +0300

    added predictor call

--- run_classification.py
+++ run_classification.py
@@ -2,25 +2,31 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('image', type=str)
-parser.add_argument('text', type=str)
+parser.add_argument('image_filename', type=str)
 
 args = parser.parse_args()
-image_directry = args.image
-text_directry = args.text
+image_filename = args.image_filename
+text_filename = str(image_filename.replace(os.path.splitext(image_filename)[1], '.txt'))
+result_filename = str(image_filename.replace(os.path.splitext(image_filename)[1], '.prediction'))
 
-print(image_directry, text_directry)
+print(image_filename, text_filename, result_filename)
 
 try:
+    raise Exception()
     # call for CONVERTIO
     # os.system('php api/request.php ' + img_file_path + ' ' + text_file_path)
 
     # call for ABBY
-    os.system('python2 abby-python-api/process.py ' + image_directry + ' ' + text_directry)
+    os.system('python2 abby-python-api/process.py ' + image_filename + ' ' + text_filename)
     print('OCR finished')
-    with open(text_directry, 'r') as f:
+    with open(text_filename, 'r') as f:
         text = f.read()
-        predict(text)
+        print('filed loaded')
+        predictions = predict(text)
+        print(predictions)
+
+    with open(result_filename, 'w') as f:
+        f.write(predictions)
 
     # call for tesaract
     # text = convert_img_to_text(img_file_path)
